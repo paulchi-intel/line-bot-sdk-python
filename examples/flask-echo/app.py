5c29dc8bbb3e140da52a12e1cdf9eb124550a726
paulchi-intel <95746021+paulchi-intel@users.noreply.github.com>
2023-03-15 17:25:08 +0800

    Update app.py

--- examples/flask-echo/app.py
+++ examples/flask-echo/app.py
@@ -67,7 +67,7 @@ def callback():
 
         line_bot_api.reply_message(
             event.reply_token,
-            TextSendMessage(text=event.message.text)
+            TextSendMessage(text="test")
         )
 
     return 'OK'
